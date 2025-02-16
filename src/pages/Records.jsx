2b37a26d57a72d008d1bf96f14f5a4d4ec4def8d
unknown <vaguaman86@gmail.com>
2025-02-16 12:51:04 +0330

    style fixes

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -2,6 +2,7 @@ import { HiOutlineMicrophone } from "react-icons/hi";
 import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import RecordsList from "../components/RecordsList";
+import { motion } from "framer-motion";
 
 function Records() {
   const navigate = useNavigate();
@@ -10,12 +11,14 @@ function Records() {
     <>
       <Header>Recordings</Header>
       <RecordsList />
-      <button
+      <motion.button
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.9 }}
         className="flex cursor-pointer items-center justify-center gap-2 rounded-full bg-purple-500 px-5 py-2 text-[20px] text-white shadow"
         onClick={() => navigate("/record")}
       >
         <HiOutlineMicrophone /> Record
-      </button>
+      </motion.button>
     </>
   );
 }
